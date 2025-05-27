3bbc6f694fc6f6baca91c985e1e57da64b11e843
sonthicse <sonthicse@gmail.com>
2025-05-27 20:51:15 +0700

    update

--- obj/Debug/net9.0-windows/BTL.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/BTL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a61b770a7423b5228ddadac934a346b2c608687")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2786bc204a7418e1c5777bb3e02f408fcf8ef90f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
